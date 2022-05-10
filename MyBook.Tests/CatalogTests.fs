81bb0b8ec46604fde495a80bf113d88efd0ce0ea
DanilMir <mirgayazov02@gmail.com>
2022-05-10 11:11:42 +0300

    add: some catalog tests

--- MyBook.Tests/CatalogTests.fs
+++ MyBook.Tests/CatalogTests.fs
@@ -5,6 +5,7 @@ open System.Text.Json
 open Microsoft.AspNetCore.Mvc.Testing
 open MyBook.Entity
 open MyBook.WebApi
+open Newtonsoft.Json
 open Xunit
 
 type responseBooks = List<Book>
@@ -31,7 +32,7 @@ type CatalogControllerTests(factory: MyBookWebApplicationFactory) =
             Assert.NotEmpty(responseJson)
 
             let responseData =
-                JsonSerializer.Deserialize<responseBooks> responseJson
+                JsonConvert.DeserializeObject<responseBooks> responseJson
 
             Assert.Equal(6, responseData.Length)
             
@@ -51,8 +52,70 @@ type CatalogControllerTests(factory: MyBookWebApplicationFactory) =
             Assert.NotEmpty(responseJson)
 
             let responseData =
-                JsonSerializer.Deserialize<responseBooks> responseJson
+                JsonConvert.DeserializeObject<responseBooks> responseJson
 
             Assert.Equal(1, responseData.Length)
-    
+            
+        [<Fact>]
+        member this.``Get book by id``() =
+            let client = this._factory.CreateClient()
+
+            let id = "8faa5631-6f76-437a-a924-1c5ad5806a5e";
+            let response =
+                client.GetAsync($"/Catalog/GetBook/{id}")
+
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+            
+            let responseJson = response.Result.Content.ReadAsStringAsync().Result
+            let responseData = JsonConvert.DeserializeObject<Book> responseJson
+            Assert.Equal("Тонкое искусство пофигизма", responseData.Title)
+            
+        [<Fact>]
+        member this.``Get book by wrong id returns NotFound``() =
+            let client = this._factory.CreateClient()
+
+            let id = "10005631-6f76-437a-a924-1c5ad5806a5e";
+            let response =
+                client.GetAsync($"/Catalog/GetBook/{id}")
+
+            Assert.Equal(HttpStatusCode.NotFound, response.Result.StatusCode)
+            
+        [<Fact>]
+        member this.``Get author by id``() =
+            let client = this._factory.CreateClient()
+
+            let id = "02788b50-5eae-42ce-a375-c0416840d687";
+            let response =
+                client.GetAsync($"/Catalog/GetAuthor/{id}")
+
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+            
+            let responseJson = response.Result.Content.ReadAsStringAsync().Result
+            let responseData = JsonConvert.DeserializeObject<Author> responseJson
+            Assert.Equal("Марк Мэнсон", responseData.FullName)
+            
+        [<Fact>]
+        member this.``Get author by wrong id returns NotFound``() =
+            let client = this._factory.CreateClient()
+
+            let id = "10005631-6f76-437a-a924-1c5ad5806a5e";
+            let response =
+                client.GetAsync($"/Catalog/GetAuthor/{id}")
+
+            Assert.Equal(HttpStatusCode.NotFound, response.Result.StatusCode)
+            
+            
+        [<Fact>]
+        member this.``Search``() =
+            let client = this._factory.CreateClient()
+
+            let id = "02788b50-5eae-42ce-a375-c0416840d687";
+            let response =
+                client.GetAsync($"/Catalog/GetAuthor/{id}")
+
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+            
+            let responseJson = response.Result.Content.ReadAsStringAsync().Result
+            let responseData = JsonConvert.DeserializeObject<Author> responseJson
+            Assert.Equal("Марк Мэнсон", responseData.FullName)
     end
