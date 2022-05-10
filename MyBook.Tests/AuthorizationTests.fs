629af2a74c92268e533eaae7ee1050cbae9475c9
DanilMir <mirgayazov02@gmail.com>
2022-05-10 08:31:24 +0300

    full AuthorizationTests

--- MyBook.Tests/AuthorizationTests.fs
+++ MyBook.Tests/AuthorizationTests.fs
@@ -17,60 +17,148 @@ type ResponseError =
       error_description: string
       error_uri: string }
 
-[<Fact>]
-let ``Register user with valid data should create new user`` () =
-    let _factory =
-        new MyBookWebApplicationFactory()
 
-    let client = _factory.CreateClient()
+[<Collection("OpenIddict tests")>]
+type AuthorizationControllerTests(factory: MyBookWebApplicationFactory) =
+    class
+        member this._factory = factory
+        
+        
+        interface IClassFixture<MyBookWebApplicationFactory>
+        
+        [<Fact>]
+        member this. ``Register user with valid data should create new user`` () =
 
-    let form =
-        [| KeyValuePair<string, string>("grant_type", "password")
-           KeyValuePair<string, string>("username", "hb4")
-           KeyValuePair<string, string>("password", "qwe123QWE_")
-           KeyValuePair<string, string>("name", "Name")
-           KeyValuePair<string, string>("lastname", "Lastname")
-           KeyValuePair<string, string>("email", "hb3@mybook.ru") |]
+            let client = this._factory.CreateClient()
 
-    let content =
-        new FormUrlEncodedContent(form)
-    content.Headers.ContentType <- Headers.MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded")
+            let form =
+                [| KeyValuePair<string, string>("grant_type", "password")
+                   KeyValuePair<string, string>("username", "hb4")
+                   KeyValuePair<string, string>("password", "qwe123QWE_")
+                   KeyValuePair<string, string>("name", "Name")
+                   KeyValuePair<string, string>("lastname", "Lastname")
+                   KeyValuePair<string, string>("email", "hb3@mybook.ru") |]
 
-    let response =
-        client.PostAsync($"/Auth/SignUp", content)
-    Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+            let content =
+                new FormUrlEncodedContent(form)
+            content.Headers.ContentType <- Headers.MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded")
 
-    let responseJson =
-        response.Result.Content.ReadAsStringAsync().Result
-    let responseData =
-        JsonSerializer.Deserialize<ResponseToken> responseJson
+            let response =
+                client.PostAsync($"/Auth/SignUp", content)
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
 
-    Assert.NotNull(responseData.access_token)
+            let responseJson =
+                response.Result.Content.ReadAsStringAsync().Result
+            let responseData =
+                JsonSerializer.Deserialize<ResponseToken> responseJson
+
+            Assert.NotNull(responseData.access_token)
 
 
-[<Fact>]
-let ``Correct Login should returns valid JWT`` () =
-    let _factory =
-        new MyBookWebApplicationFactory()
+        [<Fact>]
+        member this. ``Register user with invalid data should return BadRequest`` () =
 
-    let client = _factory.CreateClient()
+            let client = this._factory.CreateClient()
 
-    let form =
-        [| KeyValuePair<string, string>("grant_type", "password")
-           KeyValuePair<string, string>("username", "admin@mybook.ru")
-           KeyValuePair<string, string>("password", "qwe123QWE_") |]
+            let form =
+                [| KeyValuePair<string, string>("grant_type", "password")
+                   KeyValuePair<string, string>("username", "hb5")
+                   KeyValuePair<string, string>("password", "123")
+                   KeyValuePair<string, string>("name", "Name")
+                   KeyValuePair<string, string>("lastname", "Lastname")
+                   KeyValuePair<string, string>("email", "hb5@mybook.ru") |]
 
-    let content =
-        new FormUrlEncodedContent(form)
-    content.Headers.ContentType <- Headers.MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded")
+            let content =
+                new FormUrlEncodedContent(form)
+            content.Headers.ContentType <- Headers.MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded")
 
-    let response =
-        client.PostAsync($"/Auth/Login", content)
-    Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+            let response =
+                client.PostAsync($"/Auth/SignUp", content)
+            Assert.Equal(HttpStatusCode.BadRequest, response.Result.StatusCode)
+        
+        
+        [<Fact>]
+        member this. ``Register with already registered username should return BadRequest`` () =
 
-    let responseJson =
-        response.Result.Content.ReadAsStringAsync().Result
-    let responseData =
-        JsonSerializer.Deserialize<ResponseToken> responseJson
+            let client = this._factory.CreateClient()
 
-    Assert.NotNull(responseData.access_token)
+            let form =
+                [| KeyValuePair<string, string>("grant_type", "password")
+                   KeyValuePair<string, string>("username", "admin@mybook.ru")
+                   KeyValuePair<string, string>("password", "123")
+                   KeyValuePair<string, string>("name", "Name")
+                   KeyValuePair<string, string>("lastname", "Lastname")
+                   KeyValuePair<string, string>("email", "hb5@mybook.ru") |]
+
+            let content =
+                new FormUrlEncodedContent(form)
+            content.Headers.ContentType <- Headers.MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded")
+
+            let response =
+                client.PostAsync($"/Auth/SignUp", content)
+            Assert.Equal(HttpStatusCode.BadRequest, response.Result.StatusCode)
+        
+        [<Fact>]
+        member this. ``Correct Login should returns valid JWT`` () =
+            
+            let client = this._factory.CreateClient()
+
+            let form =
+                [| KeyValuePair<string, string>("grant_type", "password")
+                   KeyValuePair<string, string>("username", "admin@mybook.ru")
+                   KeyValuePair<string, string>("password", "qwe123QWE_") |]
+
+            let content =
+                new FormUrlEncodedContent(form)
+            content.Headers.ContentType <- Headers.MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded")
+
+            let response =
+                client.PostAsync($"/Auth/Login", content)
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+
+            let responseJson =
+                response.Result.Content.ReadAsStringAsync().Result
+            let responseData =
+                JsonSerializer.Deserialize<ResponseToken> responseJson
+
+            Assert.NotNull(responseData.access_token)
+            
+            
+        [<Fact>]
+        member this. ``Incorrect user name data should return BadRequest `` () =
+            
+            let client = this._factory.CreateClient()
+
+            let form =
+                [| KeyValuePair<string, string>("grant_type", "password")
+                   KeyValuePair<string, string>("username", "rick@mybook.ru")
+                   KeyValuePair<string, string>("password", "qwe123QWE_") |]
+
+            let content =
+                new FormUrlEncodedContent(form)
+            content.Headers.ContentType <- Headers.MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded")
+
+            let response =
+                client.PostAsync($"/Auth/Login", content)
+            Assert.Equal(HttpStatusCode.BadRequest, response.Result.StatusCode)
+            
+            
+            
+        [<Fact>]
+        member this. ``Incorrect password data should return BadRequest `` () =
+            
+            let client = this._factory.CreateClient()
+
+            let form =
+                [| KeyValuePair<string, string>("grant_type", "password")
+                   KeyValuePair<string, string>("username", "admin@mybook.ru")
+                   KeyValuePair<string, string>("password", "qwe123QWE") |]
+
+            let content =
+                new FormUrlEncodedContent(form)
+            content.Headers.ContentType <- Headers.MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded")
+
+            let response =
+                client.PostAsync($"/Auth/Login", content)
+            Assert.Equal(HttpStatusCode.BadRequest, response.Result.StatusCode)
+        end
--- MyBook.Tests/CatalogTests.fs
+++ MyBook.Tests/CatalogTests.fs
@@ -5,9 +5,16 @@ open Microsoft.AspNetCore.Mvc.Testing
 open MyBook.WebApi
 open Xunit
 
-[<Fact>]
-let ``Get all books`` () =
-    let _factory = new MyBookWebApplicationFactory()
-    let client = _factory.CreateClient()
-    let response = client.GetAsync($"/Catalog/GetAllBooks")
-    Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+[<Collection("Catalog tests")>]
+type CatalogControllerTests(factory: MyBookWebApplicationFactory) =
+    class
+        member this._factory = factory
+                
+        interface IClassFixture<MyBookWebApplicationFactory>
+
+        [<Fact>]
+        member this. ``Get all books`` () =
+            let client = this._factory.CreateClient()
+            let response = client.GetAsync($"/Catalog/GetAllBooks")
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+    end
